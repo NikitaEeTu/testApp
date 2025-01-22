1003aa3a1e9310f6c2954f3edd4db79000891151
NikitaEeTu <nikita71140@gmail.com>
2025-01-22 13:45:43 +0000

    inital commit

---
+++ app.js
@@ -0,0 +1,15 @@
+const name = "Nikita";
+console.log("My name is: " + name);
+
+const express = require('express')
+const app = express()
+const port = 9000
+const favFood = process.env.FOOD;
+
+app.get('/', (req, res) => {
+  res.send('Hello World! My favourity food is: ' + favFood)
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
